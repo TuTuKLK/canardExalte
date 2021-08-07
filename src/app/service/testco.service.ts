8d757314093fad5d757f1dc08630f3f912a701ac
TuTu <geant.vert42@gmail.com>
2021-08-07 22:22:47 +0200

    ajout array

--- src/app/service/testco.service.ts
+++ src/app/service/testco.service.ts
@@ -13,11 +13,17 @@ export class TestcoService {
     // return Math.floor(Math.random() * (731 - min + 1)) + min;
     return (Math.floor(Math.random() * 731)+1);
   }
+  public echantillon:any[] = [
+    69, 476, 729, 598, 644, 140, 517, 303, 487, 414, 687, 579, 522, 731, 423, 370
+  ];
   
   
 
   constructor(private _http: HttpClient) { }
   public getHero(): Observable<any>{
+    this.echantillon.forEach(element => {
+    return this._http.get(`https://www.superheroapi.com/api.php/${this.token}/${element}`);
+    });
     return this._http.get(`https://www.superheroapi.com/api.php/${this.token}/${this.idRandom()}`);
   }
 }
