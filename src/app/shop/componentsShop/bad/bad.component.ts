5bd4400ffcd05789233c527bf6a5120eeb9e534f
TuTu <geant.vert42@gmail.com>
2021-08-09 14:41:49 +0200

    tentative de tri bad.components

--- src/app/shop/componentsShop/bad/bad.component.ts
+++ src/app/shop/componentsShop/bad/bad.component.ts
@@ -11,6 +11,7 @@ export class BadComponent implements OnInit {
   public echantillon:any[] = [
     69, 476, 729, 598, 644, 140, 517, 303, 487, 414, 687, 579, 522, 731, 423, 370
   ];
+  public echantillonVilain:any[]=[];
   public nameHero:string = 'buffy';
 
   public hero = '';
@@ -29,15 +30,27 @@ export class BadComponent implements OnInit {
 
   ngOnInit(): void {
   this.echantillon.forEach(element => {
-    this._testhttp.getHeroById(element).subscribe(res=>`${this.align=res.biography.alignment}${this.hero=res.name}`);
-    // this._testhttp.getHeroById(element).subscribe(res=>this.hero=res.name);
+    this._testhttp.getHeroById(element).subscribe(res=>this.align=res.biography.alignment);
+    this._testhttp.getHeroById(element).subscribe(resp=>this.hero=resp.name);
     if(this.align==='bad'){
-      console.log(this.hero)
+      console.log(`${this.hero} est dans la liste des Vilains`);
     }
     
       // `${res.biography.alignment=="good"}`?console.log(res.name):undefined);
   });
 
+
+  // this.echantillon.forEach(element => {
+  //   this._testhttp.getHeroById(element).subscribe(res=>this.echantillonVilain.push(res.id));
+  //   this.echantillonVilain.forEach(bla => {
+  //     this._testhttp.getHeroById(bla).subscribe(resp=>console.log(this.hero=resp.name));
+  //   });
+  // });
+
+
+
+
+  
   this._testhttp.getHero().subscribe(reponse=>console.log(reponse));
   // this._testhttp.getHeroesByName("batman").subscribe(reponse=>console.log(reponse));
   
