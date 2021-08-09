bc1f84cc1739383a15f50ee995c3f0fddab54ddd
Tiilia <dosimontp@gmail.com>
2021-08-09 11:41:57 +0200

    affichage nom hero dans html

--- src/app/shop/componentsShop/good/good.component.ts
+++ src/app/shop/componentsShop/good/good.component.ts
@@ -10,19 +10,19 @@ import { APIlocalService } from 'src/app/services/apilocal.service';
   styleUrls: ['./good.component.scss']
 })
 export class GoodComponent implements OnInit {
-  
-  public echantillon:any[] = [
+
+  public echantillon: any[] = [
     69, 476, 729, 598, 644, 140, 517, 303, 487, 414, 687, 579, 522, 731, 423, 370
   ];
 
-  public Heroes:any[] = [];
-  public testHTML: string ="";
+  public Heroes: any[] = [];
+  public testHTML: any;
 
-  constructor(private _api:APIlocalService) {}
-  
+  constructor(private _api: APIlocalService) { }
+
 
   ngOnInit(): void {
-    this._api.getRandomHero().subscribe(res=>this.testHTML);
+    this._api.getRandomHero().subscribe(res => this.testHTML = res);
 
 
 
@@ -31,12 +31,12 @@ export class GoodComponent implements OnInit {
     // for (const i of this.echantillon) {
     //   this._api.getOne(i).subscribe((h)=>this.Heroes.push(h));
     // }
-  // constructor(private _testhttp:TestcoService) { }
+    // constructor(private _testhttp:TestcoService) { }
 
-  // ngOnInit(): void {
-  //   this.echantillon.forEach(element => {
-  //   this._testhttp.getHeroById(element).subscribe(res=>console.log(`${res.name} fait partie des ${res.biography.alignment=="good"?"Héros":"Vilains"}`));
-  //   });
+    // ngOnInit(): void {
+    //   this.echantillon.forEach(element => {
+    //   this._testhttp.getHeroById(element).subscribe(res=>console.log(`${res.name} fait partie des ${res.biography.alignment=="good"?"Héros":"Vilains"}`));
+    //   });
 
 
     // this.echantillon.forEach(element => {
